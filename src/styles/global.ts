e35d5b945b7bea982086f8b8598c90dad989451f
hugomatheus <hg.targino@gmail.com>
2021-03-23 18:23:27 -0300

    Componente: Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --red: #e52e4d;
         --blue: #5429cc;
+        --green: #33cc95;
         --blue-light: #6933ff;
 
         --text-title: #363f5f;
@@ -31,7 +32,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background: var(----background);
+        background: var(--background);
         -webkit-font-smoothing: antialiased;
     }
 
