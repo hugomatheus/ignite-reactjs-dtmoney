6c0fe8d34e5d878ba3ffc1b6375b084ddd6c03df
hugomatheus <hg.targino@gmail.com>
2021-06-13 19:18:48 -0300

    Carregando transações

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Modal from "react-modal";
 import { useState } from "react";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
@@ -20,7 +20,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal 
@@ -29,6 +29,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
